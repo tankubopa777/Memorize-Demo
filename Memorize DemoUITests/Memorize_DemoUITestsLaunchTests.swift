1f79b798f16bf7b313775fa0831a5d55beb171f3
ワルンポ-ンさん <tankubopa77@gmail.com>
2023-10-10 15:11:07 +0700

    Initial Commit

---
+++ Memorize DemoUITests/Memorize_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Memorize_DemoUITestsLaunchTests.swift
+//  Memorize DemoUITests
+//
+//  Created by warunporn intarachana on 10/10/2566 BE.
+//
+
+import XCTest
+
+final class Memorize_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
